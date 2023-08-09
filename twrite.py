1afd4a8f62df04adfed7b468a849b362f416c9b6
NandanReddyP <parnapalli2004@gmail.com>
2023-08-10 03:47:05 +0530

    commit

--- twrite.py
+++ twrite.py
@@ -5,17 +5,22 @@ responses = open('./csv files/trans.txt','w',newline='')
 write = csv.writer(responses)
 
 doc = fitz.open('./pdf files/trans.pdf') # open a document
-i=0
-text = chr(12).join([page.get_text() for page in doc])
+text = chr(12).join([page.get_text() for page in doc]) # text in doc
+
 lines = text.strip().split('\n')
 for line in lines:
     index = lines.index(line)
     line = line.replace("", "")
-    if i==1: write.writerow([line])
-    if i==3: write.writerow([line[10:]])
-    if i>10 and i%2!=0:
+    if index==1: write.writerow([line])
+    if index==3: write.writerow([line[10:]])
+    if index>10 and index%2!=0:
         Qid = line
         Res = lines[index+1]
         if Res!='Unanswered':
-            write.writerow([Qid, '$'.join(Res.split(','))])
-    i+=1
+            if Qid[:4]==Res[:4]:
+                write.writerow([Qid, '$'.join(Res.split(','))])
+            else:
+                write.writerow([Qid, Res])
+
+responses.close()
+doc.close()
